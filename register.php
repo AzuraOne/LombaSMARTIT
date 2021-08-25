de16ef1236cac4bb87516d7a27f5bd90500b7a39
AzuraOne <narutokah1@gmail.com>
2021-08-25 22:06:36 +0700

    Menambah fitur remember me dan register.php

---
+++ register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register Page</title>
+</head>
+<body>
+    <form action="registerinfo.php" method="post">
+        <ul style="list-style-type: none;">
+            <li>
+                <label for="nama">Nama</label>
+                <input type="text" name="nama" id="nama">
+            </li>
+            <li>
+                <label for="password">Nama</label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <label for="Email">Nama</label>
+                <input type="email" name="email" id="email">
+            </li>
+            <li>
+                <button type="submit">Kirim</button>
+            </li>
+        </ul>
+
+    </form>
+    
+</body>
+</html>
