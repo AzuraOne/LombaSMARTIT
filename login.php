2c75c376f3a22aaf23fb4d6be3436ac0f28d610b
AzuraOne <narutokah1@gmail.com>
2021-08-25 21:53:13 +0700

    Membuat halaman login secara sederhana

--- login.php
+++ login.php
@@ -7,6 +7,20 @@
     <title>Login Page</title>
 </head>
 <body>
+    <form action="loginprocess.php" method="post">
+        <ul style="list-style-type: none;">
+            <li>
+                <label for="username">Masukkan Username</label>
+                <input type="text" name="username" id="username">
+
+            </li>
+            <li><label for="password">Masukkan Password</label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li><button type="submit">Kirim</button></li>
+        </ul>
+
+    </form>
     
     
 </body>
