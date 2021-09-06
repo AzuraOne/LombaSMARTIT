6c4a8610656d4e57fd542f4b02671bb11c3b8bb8
AzuraOne <narutokah1@gmail.com>
2021-09-07 01:08:03 +0700

    menambahkan mysli_fetch_array

--- loginprocess.php
+++ loginprocess.php
@@ -5,14 +5,18 @@ require "koneksi.php";
     $password = $_POST['password'];
     $query = "SELECT * from akun where nama='$username' AND password='$password'";
     $ceklogin = mysqli_query($con, $query);
+    $quer = mysqli_fetch_array($ceklogin);
     $affectedrows = mysqli_affected_rows($con);
     if ($affectedrows > 0) {
-        if ($username == $query['nama']) {
-            if ($password == $query['password']) {
-                
+        if ($username == $quer['nama']) {
+            if ($password == $quer['password']) {
+                echo "Berhasil Login";
+
+            }else {
+                echo "Password Salah";
             }
         }else {
-            echo "Password Salah";
+            echo "Username Salah";
         }
         
     }else {
